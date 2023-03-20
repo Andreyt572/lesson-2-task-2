5c535c880ecba5f8dc6ffb5c6f42b0b7b69c5f28
Andrey Tolokonnikov <andrey572@mail.ru>
2023-03-20 18:29:19 +0300

    commit 5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 150;
-        int payment = 400;
+        int payment = 155112;
         int bonus = 1;
 
         if (payment > 1000) {
-            System.out.println((balance + payment) / 100 * bonus);
-            System.out.println(balance + payment);
+            System.out.println("Количество бонусов " + (balance + payment) / 100 * bonus);
+            System.out.println("Общий баланс после пополнения " + (balance + payment));
         } else {
             System.out.println (balance + payment);
         }

168cdef78ad809be0974963939709f0e1290947e
Andrey Tolokonnikov <andrey572@mail.ru>
2023-03-20 12:47:28 +0300

    commit 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 150;
-        int payment1 = 1001;
-        int payment2 = 500;
+        int balance = 150;
+        int payment = 400;
         int bonus = 1;
 
-        if (payment1 > 1000) {
-            System.out.println((x + payment1) / 100 * bonus);
-        }
-        if (payment2 < 1000) {
-            System.out.println(x + payment2);
+        if (payment > 1000) {
+            System.out.println((balance + payment) / 100 * bonus);
+            System.out.println(balance + payment);
+        } else {
+            System.out.println (balance + payment);
         }
 
     }
